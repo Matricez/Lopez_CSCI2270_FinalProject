ef28e3c29ba54b22f4d8e11fc6095473f832012d
user <lopezlorenzo163@gmail.com>
2017-07-24 09:46:09 -0600

    Adding files

---
+++ AsteroidMain.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "AsteroidMining.cpp"
+#include <sstream>
+#include <fstream>
+
+
+int main(int argc, char** argv)
+{
+
+
+
+
+}
---
+++ AsteroidMining.hpp
@@ -0,0 +1,13 @@
+#ifndef ASTEROIDMINING_HPP
+#define ASTEROIDMINING_HPP
+//#include "MovieTree.cpp"
+#include <string>
+
+
+
+
+
+
+
+
+#endif
